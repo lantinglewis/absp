3ff0934c17eee29aca82921af9c71cc012f8ddaa
lantinglewis <lewislanting@hotmail.com>
2022-12-07 19:40:40 +0100

    chapter 2 and 3

---
+++ chapter_03/collatz_sequence.py
@@ -0,0 +1,14 @@
+def collatz(number):
+
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+    elif number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
+
+
+n = int(input('Enter number:\n'))
+while n != 1:
+    n = collatz(n)
